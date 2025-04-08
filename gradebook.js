99cb152ab1da73e38d35e0f0d2dd8f9228cee9bf
bleebusmeebs <acjackson7@my.waketech.edu>
2025-04-08 15:39:08 -0400

    Added the grade book files
    The grade book lesson

---
+++ gradebook.js
@@ -0,0 +1,8 @@
+function fetchGradeData(){
+    console.log("fetching grade data...");
+}
+function populateGradebook(data){
+    console.log("Populating gradebook with data:", data);
+}
+const gradeData = fetchGradeData();
+populateGradebook(gradeData)
