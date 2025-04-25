c7aa17cd033cfb95f13ba2cbef56690770862775
bleebusmeebs <acjackson7@my.waketech.edu>
2025-04-24 21:16:14 -0400

    Updated

--- gradebook.js
+++ gradebook.js
@@ -1,8 +1,36 @@
 function fetchGradeData(){
     console.log("fetching grade data...");
-}
+    let xhr = new XMLHttpRequest();
+    let apiRoute = "/api/grades";
+    xhr.onreadystatechange = function(){
+        let results;
+        if(xhr.readyState === xhr.DONE){
+            if(xhr.status !== 200){
+                console.error('Sorry there was an error. Status: ${xhr.status}' )
+            }
+              populateGradebook{JSON.parse(xhr.responseText)};
+            };
+        
+    }.bind(this);
+    xhr.open("get", apiRoute, true);
+    xhr.send();
+    
+};
 function populateGradebook(data){
     console.log("Populating gradebook with data:", data);
+    let tableEln = document.getElementById("gradebook");
+        data.forEach{function(assignment){
+            let row = document.createElement("tr");
+            let columns = []
+            columns.name = document.createElement("td");
+            columns.name.appendChild(
+                document.createTextNode(assignment.total_grade)
+
+            );
+            row.appendChild(columns.name);
+            row.appendChild(columns.grade);
+            tableEln.appendChild(row);
+        }};
 }
 const gradeData = fetchGradeData();
 populateGradebook(gradeData)
